3821863c564ef7e577c8f62a95e5aed32f88c9c2
Noah Smith <thucer@duck.com>
2022-06-23 12:25:18 -0700

    Removed oddly named directory, replaced with more readable name, no changes to code

--- assignments/WindowMaster/src/main/java/com/mycompany/windowmaster/WindowMaster.java
+++ assignments/WindowMaster/src/main/java/com/mycompany/windowmaster/WindowMaster.java
@@ -12,19 +12,50 @@ import java.util.Scanner;
 /**
  *
  * @author John
- * 6/12/22
- * A note on usage, this program seems to work in most cases however, if the user,
- * for whatever reason, enters a numeral value followed by the character f such as,
- * '4f' or '3f' or any other combination in that pattern it is not caught by the catch
- * block. I believe this is because floats are often defined with a numeral followed
- * by the f character. I am unsure how to correct this, though it does not impact 
- * functionality as the program still reads '3f' as a float value of 3.
  */
 public class WindowMaster {
 
     //Scanner is declared as a class variable as multiple methods require it
     Scanner input = new Scanner(System.in);
-    //gets dimensions from user
+    //gets float info from user, ensures entries are not 0
+    public float askQuestions(String question){
+        float output;
+        //switch statement used as one quetsion deals with ints not floats
+        switch(question){
+            case "How many windows will be installed: ":
+                    System.out.print(question);
+                    output = Float.parseFloat(input.nextLine());
+                    while(output == 0)
+                    {
+                        System.out.println("Sorry 0 is not a valid amount to "
+                                + "install. " + question);
+                        output = Float.parseFloat(input.nextLine());
+                    }
+                /*returns used here instead of break in order to pass control back to caller method */
+                return output;
+            case "What is the cost of glass per square foot: ":
+            case "What is the cost of trim per linear foot: ":
+                System.out.print(question);
+                    output = Float.parseFloat(input.nextLine());
+                    while(output == 0)
+                    {
+                        System.out.println("Sorry $0.00 is not a valid cost value. "
+                                + question);
+                        output = Float.parseFloat(input.nextLine());
+                    }
+                return output;
+            default:
+                   System.out.print(question);
+                    output = Float.parseFloat(input.nextLine());
+                    while(output == 0)
+                    {
+                        System.out.println("Sorry 0 feet is not a valid dimensional "
+                    + "value. " + question);
+                        output = Float.parseFloat(input.nextLine());
+                    }    
+        }
+        return output;
+    }
     public float[] getDimensions()
     {
         float height = 0, width = 0;
@@ -33,13 +64,13 @@ public class WindowMaster {
         do{
           try{
             if(!firstParam){
-            System.out.print("What is the window height, in feet: ");
-            height = Float.parseFloat(input.nextLine());
-            //Evaluates whether the first parameter, cost, meets the guidelines
+            //System.out.print("What is the window height, in feet: ");
+            height = askQuestions("What is the window height, in feet: ");
+            //Evaluates whether the first parameter, height, meets the guidelines
             firstParam = true;
             }
-            System.out.print("What is the window width, in feet: ");
-            width = Float.parseFloat(input.nextLine());
+            //System.out.print();
+            width = askQuestions("What is the window width, in feet: ");
             //Only passes when both statements are valid
             isAFloat = true;
           }catch (NumberFormatException e){
@@ -59,12 +90,12 @@ public class WindowMaster {
         do{
           try{
             if(!firstParam){  
-            System.out.print("What is the cost of glass per square foot: ");
-            glassC = Float.parseFloat(input.nextLine());
+            //System.out.print("What is the cost of glass per square foot: ");
+            glassC = askQuestions("What is the cost of glass per square foot: ");
             firstParam = true;
             }
-            System.out.print("What is the cost of trim per linear foot: ");
-            trimC = Float.parseFloat(input.nextLine());
+            //System.out.print("What is the cost of trim per linear foot: ");
+            trimC = askQuestions("What is the cost of trim per linear foot: ");
             isAFloat = true; 
           }catch (NumberFormatException e){
               System.out.println("Sorry, that was not a valid float value, please "
@@ -78,12 +109,16 @@ public class WindowMaster {
     //gets number of windows
     public int getNumber()
     {
+        float number;
         int numWin = 0;
         boolean isANum = false;
         do{
            try{
-            System.out.print("How many windows will be installed: ");
-            numWin = Integer.parseInt(input.nextLine());
+           //System.out.print("How many windows will be installed: ");
+            number = askQuestions("How many windows will be installed: ");
+            //TESTSystem.out.println("The number of windows is number: "+number);
+            numWin = (int)number;
+            //TESTSystem.out.println("The number of windows after conversion "+numWin);
             isANum = true;
            }catch (NumberFormatException e){
                System.out.println("Sorry, that was not a valid integer, please "
